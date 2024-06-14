60882b4cb7c027c08100a35089eb5721cab10604
LeTruongLam <104148284+LeTruongLam@users.noreply.github.com>
2024-06-14 14:13:03 +0700

    be

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 import mysql from "mysql2";
 export const db = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "962002",
-  database: "card",
+  host: process.env.DB_HOST, 
+  user: process.env.DB_USERNAME, 
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
 });
